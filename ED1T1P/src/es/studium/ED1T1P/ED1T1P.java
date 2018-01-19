06bfbd4c682faece9717bcb099449a095fa24aa3
studium <studium@DESKTOP-CGGB52H>
2018-01-19 13:43:47 +0100

    terminacion

---
+++ ED1T1P/src/es/studium/ED1T1P/ED1T1P.java
@@ -0,0 +1,23 @@
+package es.studium.ED1T1P;
+
+import java.util.Random;
+
+public class ED1T1P {
+	static Random rnd=new Random();
+
+	public static void main(String[] args)
+	{
+		int veces; 
+		int contador=0;
+		veces = rnd.nextInt(100)+1;
+		for(int i=0; i<veces;i++)
+			{
+			
+			System.out.println(rnd.nextInt(50));
+			contador++;
+
+			}
+		System.out.println("Se han generado "+contador+" números aleatorios");
+	}
+
+}
